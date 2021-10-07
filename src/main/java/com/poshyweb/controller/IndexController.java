d52e886ebef7d4c7547887f52d96a52bbad553dc
RobertoAraujo <endriosrobert@gmail.com>
2021-10-07 09:26:30 -0500

    estrutura mvc no projeto

---
+++ src/main/java/com/poshyweb/controller/IndexController.java
@@ -0,0 +1,5 @@
+package com.poshyweb.controller;
+
+public class IndexController {
+
+}
---
+++ src/main/java/com/poshyweb/modelo/Produto.java
@@ -0,0 +1,10 @@
+package com.poshyweb.modelo;
+
+import java.io.Serializable;
+
+public class Produto implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+
+}
